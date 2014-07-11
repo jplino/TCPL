61c1042bd5cbd48c839e542dc0ba7464408aae7c
jplino <joao.lino@outlook.com>
2014-07-11 21:21:52 +0100

    Exercises

---
+++ CHAPTER1/1-15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float celcius (int f);
+main()
+{
+	int i;	
+
+	for (i = 0; i <= 300; i = i + 20)
+		printf("%d\t%6.1f\n", i, celcius(i));
+
+	return 0;
+}
+
+float celcius(int fahr)
+
+{
+	float p;
+
+	p = (5.0/9.0) * (fahr-32);
+
+	return p;
+}
+	
